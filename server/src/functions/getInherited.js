886ea7f7c52ec3bc282b614bef55f2eeade6073b
teo67 <teogerst@gmail.com>
2022-06-09 11:01:25 -0700

    typos

--- server/src/functions/getInherited.js
+++ server/src/functions/getInherited.js
@@ -1,22 +1,21 @@
 const checkVar = require('./checkVar.js');
 const findInVariable = require('./findInVariable.js');
 const getInherited = (rt, dep, playground = false) => {
-    //console.log('depending');
+    //console.log('getting inherited to ' + rt.raw);
     let inherited = exporting.dep(dep, rt);
     if(inherited === null) {
         // didnt work
         return null;
     }
     inherited = inherited[inherited.length - 1];
-    if(checkVar(inherited, dep, null, "", playground)) {
+    if(!checkVar(inherited, dep, null, "", playground)) {
         return null;
     }
     const asProto = findInVariable("prototype", inherited.properties, null);
     if(asProto === null) {
         return inherited;
     }
-    if(checkVar(asProto, dep, inherited, "prototype", playground)) {
-        // wait for it
+    if(!checkVar(asProto, dep, inherited, "prototype", playground)) {
         return null;
     }
     // we chillin
